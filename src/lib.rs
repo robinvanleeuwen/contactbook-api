cc2d7e3838ad6cab0240a9aa2b22ff3666c593d2
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2019-04-20 23:32:15 +0200

    Make docker compatible. Performance optimalisations

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,51 @@ extern crate dotenv;
 
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
-use dotenv::dotenv;
+
 use std::env;
 
 pub fn establish_connection() -> Result<PgConnection, ConnectionError> {
-    dotenv().ok();
+    dotenv::from_filename("/etc/contactbook-api.conf").ok().unwrap_or_default();
 
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
     println!("Connecting to database.");
     PgConnection::establish(&database_url)
+}
+
+pub fn get_listen_address() -> String {
+
+    dotenv::from_filename("/etc/contactbook-api.conf").ok().unwrap_or_default();
+    let listen_address = match env::var("LISTEN_ADDRESS") {
+        Ok(v) => v,
+        Err(_) => {
+            eprint!("No listen address found, using 127.0.0.1\n");
+            String::from("127.0.0.1")
+        }
+    };
+
+    listen_address.to_owned()
+
+}
+
+pub fn get_listen_port<'a>() -> u16 {
+
+    dotenv::from_filename("/etc/contactbook-api.conf").ok().unwrap_or_default();
+
+    let listen_port = match env::var("LISTEN_PORT") {
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("No listen port found, using 3030\n");
+            String::from("3030")
+        }
+    };
+
+    let port = match listen_port.parse::<u16>() {
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("Could not parse port, using 3030");
+            3030
+        }
+    };
+    port
 }
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,16 @@ use serde_json::*;
 use serde::{Serialize, Deserialize};
 use jsonrpc_core::*;
 
-extern crate contacts;
+extern crate contactbook_api;
 extern crate diesel;
-extern crate notifica;
 
-use contacts::*;
+use contactbook_api::*;
 use self::models::*;
 use diesel::prelude::*;
 use diesel::insert_into;
 use diesel::delete;
+use jsonrpc_http_server::DomainsValidation;
+use jsonrpc_http_server::AccessControlAllowOrigin;
 
 fn main() {
 
@@ -20,8 +21,8 @@ fn main() {
 
     io = add_handlers(io);
 
-    let address = "127.0.0.1";
-    let port: u16 = 3030;
+    let address = get_listen_address();
+    let port: u16 = get_listen_port();
 
     let socket_address = &SocketAddr::new(
         (address).parse().unwrap(), port
@@ -29,15 +30,12 @@ fn main() {
 
     let server = ServerBuilder::new(io)
         .threads(5)
+        .cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Any]))
         .start_http(socket_address)
         .expect("Unable to start server.");
 
     println!("Starting server on {}:{}...", address, port);
 
-    match notifica::notify("Starting a new server", format!("Listening on {}:{}", address, port).as_str()) {
-        Err(e) => eprintln!("{}",e),
-        _ => println!("DBUS OK"),
-    }
     server.wait();
 }
 
@@ -48,7 +46,7 @@ fn add_handlers(mut io: IoHandler) -> IoHandler {
     io.add_method("create", create_contact);
     io.add_method("delete", delete_contact);
     io.add_method("search", show_contacts);
-    return io
+    io
 }
 
 
@@ -58,7 +56,7 @@ fn delete_contact(params: Params) -> jsonrpc_core::Result<jsonrpc_core::types::V
     let connection = match establish_connection() {
         Ok(c) => c,
         Err(_) => {
-            let error = format!("500 ERROR: Could not connect to database.").to_string();
+            let error = "500 ERROR: Could not connect to database.".to_string();
             return Ok(Value::String(error));
         }
     };
@@ -85,9 +83,7 @@ fn show_contacts(params: Params) -> jsonrpc_core::Result<jsonrpc_core::types::Va
     let connection = match establish_connection() {
         Ok(c) => c,
         Err(_) => {
-            let error = format!(
-                "500 ERROR: Could not connect to database."
-            ).to_string();
+            let error = "500 ERROR: Could not connect to database.".to_string();
             return Ok(Value::String(error));
         }
     };
@@ -119,7 +115,7 @@ fn show_contacts(params: Params) -> jsonrpc_core::Result<jsonrpc_core::types::Va
 
     for c in &result { cts.push(c); }
 
-    if cts.len() > 0 {
+    if !cts.is_empty() {
         data = json!(
             {
                 "contacts": cts
@@ -137,7 +133,7 @@ fn create_contact(params: Params) -> jsonrpc_core::Result<jsonrpc_core::types::V
     let connection = match establish_connection() {
         Ok(c) => c,
         Err(_) => {
-            let error = format!("500 ERROR: Could not connect to database.").to_string();
+            let error = "500 ERROR: Could not connect to database.".to_string();
             return Ok(Value::String(error));
         }
     };
@@ -155,11 +151,11 @@ fn create_contact(params: Params) -> jsonrpc_core::Result<jsonrpc_core::types::V
     let p: CreateContactParams = params.parse()?;
 
     let cname    = p.name;
-    let cphone1  = p.phone1.unwrap_or("".to_string());
-    let cphone2  = p.phone2.unwrap_or("".to_string());
-    let cphone3  = p.phone3.unwrap_or("".to_string());
-    let cemail   = p.email.unwrap_or("".to_string());
-    let ccomment = p.comment.unwrap_or("".to_string());
+    let cphone1  = p.phone1.unwrap_or_else(|| "".to_string());
+    let cphone2  = p.phone2.unwrap_or_else(|| "".to_string());
+    let cphone3  = p.phone3.unwrap_or_else(|| "".to_string());
+    let cemail   = p.email.unwrap_or_else(|| "".to_string());
+    let ccomment = p.comment.unwrap_or_else(|| "".to_string());
 
     let new_contact = CreateContact {
         name: cname,
